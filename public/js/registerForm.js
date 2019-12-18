7fef77bb17f65757b0927fffe3902d1b0c48ce22
bohdanfinology <bogdan.melnik@brokers.expert>
2019-12-18 17:29:06 +0200

    updated user-create popup

--- public/js/registerForm.js
+++ public/js/registerForm.js
@@ -16,65 +16,127 @@ $(document).ready(function(){
 
 // Merchant window
 
+const curentUserId = document.querySelector('.curentUserId').textContent;
+const currentUserTypeClass = document.querySelector('.currentUserTypeClass').textContent;
 const merchantChooseBtn = document.querySelector('.merchantChooseList');
 let showIndicator = true;
 
-merchantChooseBtn.addEventListener('click', () => {
-    document.querySelector('.selectAllBox').innerHTML = 'Loading...';
-    if (showIndicator) {
-        let fetchPromise  = fetch('http://18.216.223.81:3000/getMerchants');
-        fetchPromise.then(response => {
-            return response.json();
-            }).then(merchants => {
+if ( currentUserTypeClass === 'admin' ) {
 
-                class MerchantOptoinList {
-                    constructor(){
-                        this.list = merchants;
-                        this.render();
-                    }
-
-                    loadMerchant(list) {
-
-                        this.container = document.querySelector('.selectAllBox');
-                        this.container.innerHTML = ` 
-                            <span class="mechCheckBox"> <input onclick="selectAll(this)" type="checkbox" name="select-all" id="selectAll "> Select / Remove All</span>
-                        `;
-
-                        list.slice(0, list.length).forEach((item, i) => {
-                            this.span = document.createElement("span");
-                            this.span.className = `mechCheckBox`;
-                            this.span.innerHTML =  `
-                                <span class="mechCheckBox"><input class="check" type="checkbox" name='merchant' value=${item._id }> ${item.name}</span>
+    merchantChooseBtn.addEventListener('click', () => {
+        document.querySelector('.selectAllBox').innerHTML = 'Loading...';
+        if (showIndicator) {
+            let fetchPromise  = fetch('http://18.216.223.81:3000/getMerchants'); // in settlements.js route file
+            fetchPromise.then(response => {
+                return response.json();
+                }).then(merchants => {
+    
+                    class MerchantOptoinList {
+                        constructor(){
+                            this.list = merchants;
+                            this.render();
+                        }
+    
+                        loadMerchant(list) {
+    
+                            this.container = document.querySelector('.selectAllBox');
+                            this.container.innerHTML = ` 
+                                <span class="mechCheckBox"> <input onclick="selectAll(this)" type="checkbox" name="select-all" id="selectAll "> Select / Remove All</span>
                             `;
-                            this.leftBox =  document.querySelector('.leftBox');
-                            this.rightBox = document.querySelector('.rightBox');
-                            if ( i % 2 === 0) {
-                                this.leftBox.append(this.span);
-                            } else {
-                                this.rightBox.append(this.span);
-                            }
-                            
-                        });
-                    }
+    
+                            list.slice(0, list.length).forEach((item, i) => {
+                                this.span = document.createElement("span");
+                                this.span.className = `mechCheckBox`;
+                                this.span.innerHTML =  `
+                                    <span class="mechCheckBox"><input class="check" type="checkbox" name='merchant' value=${item._id }> ${item.name}</span>
+                                `;
+                                this.leftBox =  document.querySelector('.leftBox');
+                                this.rightBox = document.querySelector('.rightBox');
+                                if ( i % 2 === 0) {
+                                    this.leftBox.append(this.span);
+                                } else {
+                                    this.rightBox.append(this.span);
+                                }
+                                
+                            });
+                        }
+    
+                        render(){
+                            this.loadMerchant(this.list);
+                        }
+                    };
+    
+                const a = new MerchantOptoinList(merchants);
+    
+                
+            });
+            showIndicator = false; 
+        } else {
+            showIndicator = true;
+            document.querySelector('.selectAllBox').innerHTML = '';
+            document.querySelector('.leftBox').innerHTML = '';
+            document.querySelector('.rightBox').innerHTML = '';
+        }
+    });
+} else {
 
-                    render(){
-                        this.loadMerchant(this.list);
-                    }
-                };
+    merchantChooseBtn.addEventListener('click', () => {
 
-            const a = new MerchantOptoinList(merchants);
-
-            
-        });
-        showIndicator = false; 
-    } else {
-        showIndicator = true;
-        document.querySelector('.selectAllBox').innerHTML = '';
-        document.querySelector('.leftBox').innerHTML = '';
-        document.querySelector('.rightBox').innerHTML = '';
-    }
-});
-
+        document.querySelector('.selectAllBox').innerHTML = 'Loading...';
+        if (showIndicator) {
+            let fetchPromise  = fetch('http://18.216.223.81:3000/getMerchant/' + curentUserId); // in settlements.js route file
+            fetchPromise.then(response => {
+                return response.json();
+                }).then(merchants => {
+                    console.log(merchants);
+                    class MerchantOptoinList {
+                        constructor(){
+                            this.list = merchants;
+                            this.render();
+                        }
+    
+                        loadMerchant(list) {
+    
+                            this.container = document.querySelector('.selectAllBox');
+                            this.container.innerHTML = ` 
+                                <span class="mechCheckBox"> <input onclick="selectAll(this)" type="checkbox" name="select-all" id="selectAll "> Select / Remove All</span>
+                            `;
+    
+                            list.slice(0, list.length).forEach((item, i) => {
+                                this.span = document.createElement("span");
+                                this.span.className = `mechCheckBox`;
+                                this.span.innerHTML =  `
+                                    <span class="mechCheckBox"><input class="check" type="checkbox" name='merchant' value=${item._id }> ${item.name}</span>
+                                `;
+                                this.leftBox =  document.querySelector('.leftBox');
+                                this.rightBox = document.querySelector('.rightBox');
+                                if ( i % 2 === 0) {
+                                    this.leftBox.append(this.span);
+                                } else {
+                                    this.rightBox.append(this.span);
+                                }
+                                
+                            });
+                        }
+    
+                        render(){
+                            this.loadMerchant(this.list);
+                        }
+                    };
+    
+                const a = new MerchantOptoinList(merchants);
+    
+                
+            });
+            showIndicator = false; 
+        } else {
+            showIndicator = true;
+            document.querySelector('.selectAllBox').innerHTML = '';
+            document.querySelector('.leftBox').innerHTML = '';
+            document.querySelector('.rightBox').innerHTML = '';
+        }
+    });
+}
 
 const selectAll = (source) => {
     checkboxes = document.getElementsByName('merchant');
